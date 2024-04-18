26a8081e85eb93363b69c0f73404cafb382419a4
yush-nh <yushin.nihei@luxiar.com>
2024-04-18 12:02:36 +0900

    modified to keep id

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'json'
 require 'cgi'
 
 FILE_PATH = 'public/memos.json'
-next_id = 1
+next_id = File.read('public/next_id.txt').to_i
 
 def collect_memos
   File.open(FILE_PATH) { |f| JSON.parse(f.read) }
@@ -56,6 +56,7 @@ post '/memos' do
   memos[next_id] = { 'title' => params[:title], 'content' => params[:content] }
   save_memos(memos)
   next_id += 1
+  File.write('public/next_id.txt', next_id)
 
   redirect '/memos'
 end
